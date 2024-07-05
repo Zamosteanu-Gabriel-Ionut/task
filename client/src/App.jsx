eeabc9cf65536e8018576a72a09f2f025987c4b1
Zamosteanu Gabriel-Ionut <zamosteanugabrielionut@yahoo.com>
2024-07-05 18:18:54 +0300

    last change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import axios from 'axios';
-import {Toaster} from 'react-hot-toast' 
+import {Toaster} from 'react-hot-toast';
+import { UserContextProvider } from '../context/userContext';
 
 axios.defaults.baseURL ='http:/localhost:8000';
 axios.defaults.withCredentials = true;
@@ -13,15 +14,16 @@ axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/login' element={<Login />} />
-    </Routes>
-    </>
+    <UserContextProvider>
+      <Navbar />
+      <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </UserContextProvider>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dentv = require('dotenv').config()   
 const cors = require('cors');
 const {mongoose} = require('mongoose');
+const cookieParser  = require('cookie-parser');
 const app= express();
 // database connection
 mongoose.connect(process.env.MONGO_URL)
@@ -9,9 +10,11 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err) => console.log('Database not connected', err))
 
 //middleware
-app.use(express.json()) 
+app.use(express.json()) ;
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 
-app.use('/' , require('./routes/authRoutes'))
+app.use('/' , require('./routes/authRoutes'));
 
 const port = 8000;
 app.listen(port, () =>  console.log(`Server is running on port ${port}`))   
